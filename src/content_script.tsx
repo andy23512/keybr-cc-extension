a2e4db1a33578c46c166f3daa827fefc624d1e41
Tangent Chang <andy23512@gmail.com>
2025-10-18 11:57:10 +0800

    feat: adjust position of layout

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -4,10 +4,9 @@ import AppComponent from "./component/app.component";
 
 const appContainer = document.createElement("div");
 appContainer.id = "keybr-cc-extension-root";
-appContainer.style.position = "absolute";
-appContainer.style.top = "calc(100vh - 16rem + 3rem)";
-appContainer.style.left = "50%";
-appContainer.style.transform = "translateX(-50%)";
+appContainer.style.position = "relative";
+appContainer.style.width = "100%";
+appContainer.style.height = "0px";
 appContainer.style.zIndex = "1";
 document.body.appendChild(appContainer);
 
