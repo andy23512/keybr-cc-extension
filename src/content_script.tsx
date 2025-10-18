1bbae2b9422908fd1c8ded0a34757e26e0155158
Tangent Chang <andy23512@gmail.com>
2025-10-18 09:29:09 +0800

    feat: change position

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,70 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppComponent from "./component/app.component";
-let currentText: string | null = null;
-const CHARACTER_TO_POS = {
-  w: 6,
-  g: 8,
-  z: 9,
-  k: 11,
-  v: 12,
-  m: 13,
-  c: 14,
-  r: 16,
-  e: 19,
-  i: 21,
-  o: 24,
-  u: 29,
-  b: 51,
-  x: 52,
-  q: 54,
-  f: 56,
-  p: 57,
-  h: 58,
-  d: 59,
-  a: 61,
-  "": 63,
-  t: 64,
-  l: 66,
-  j: 68,
-  n: 69,
-  y: 71,
-  s: 74,
-};
-function getCurrentText() {
-  const text = document.querySelector('div[dir="ltr"] span[class]');
-  let nextCurrentText = text ? text.textContent : null;
-  if (currentText !== nextCurrentText) {
-    currentText = nextCurrentText;
-    updateHighlight();
-  }
-}
-setInterval(getCurrentText, 100);
-updateHighlight();
+
 const appContainer = document.createElement("div");
 appContainer.id = "keybr-cc-extension-root";
+appContainer.style.position = "absolute";
+appContainer.style.top = "calc(100vh - 16rem + 3rem)";
+appContainer.style.left = "50%";
+appContainer.style.transform = "translateX(-50%)";
+appContainer.style.zIndex = "1";
 document.body.appendChild(appContainer);
+
 const root = ReactDOM.createRoot(appContainer);
 root.render(
   <React.StrictMode>
     <AppComponent />
   </React.StrictMode>
 );
-
-function updateHighlight() {
-  document
-    .querySelectorAll('[data-type="highlight"]')
-    .forEach((e) => e.setAttribute("opacity", "0"));
-  if (currentText) {
-    const positionCode =
-      CHARACTER_TO_POS[currentText as keyof typeof CHARACTER_TO_POS];
-    if (!positionCode) {
-      return;
-    }
-    document
-      .querySelector(
-        `[ng-reflect-position-code="${positionCode}"] [data-type="highlight"]`
-      )
-      ?.setAttribute("opacity", "0.5");
-  }
-}
