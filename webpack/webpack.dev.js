aa0077738e827dd7bfde796a33228fb8800744d1
Tangent Chang <andy23512@gmail.com>
2025-11-25 20:57:56 +0800

    fix: fix missing live reload plugin error

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const LiveReloadPlugin = require("webpack-livereload-plugin");
 
 module.exports = merge(common, {
   devtool: "inline-source-map",
